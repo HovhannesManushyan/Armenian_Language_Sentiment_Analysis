fa9ab9e9d107ea1fd9affcc29cabe612a0c17968
Rafael-Sargs <62501198+Rafael-Sargs@users.noreply.github.com>
2020-03-29 13:35:16 +0400

    Google translate code

--- WriteInOneFile/write_in_file.py
+++
@@ -1,9 +0,0 @@
-﻿import os
-output = ''
-for file in os.listdir('test_neg/'):
-    with open('test_neg/'+file, "r",encoding="utf-8") as f:
-        content = f.read()
-    output += content + '\n'
-output_bytes = bytes(output, encoding = 'utf-8')
-with open('final.txt', 'wb') as f:
-    f.write(output_bytes)
